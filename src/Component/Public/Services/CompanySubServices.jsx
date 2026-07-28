0853679b5d26b445b3ee1395fc0d0284eba7e458
Rohit Dharmik <rohit1234dharmik@gmail.com>
2026-07-28 18:42:03 +0530

    update

--- src/Component/Public/Services/CompanySubServices.jsx
+++ src/Component/Public/Services/CompanySubServices.jsx
@@ -54,7 +54,7 @@ import { toast } from "sonner";
 const fetchContractors = async (serviceId, userId) => {
   if (!serviceId) return [];
   return (
-    (await userServicesdetailsGetByParam(`serviceId=${0}?userId=${userId}?U serType=1`)) ??
+    (await userServicesdetailsGetByParam(`serviceId=${0}&userId=${userId}&userType=1`)) ??
     []
   );
 };
